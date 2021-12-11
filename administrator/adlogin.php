88ca1e51a7faae15b05e3b302a01e1ee2e2aa04f
unknown <shirley.yankyera2@gmail.com>
2021-12-11 14:58:38 +0000

    error correction

--- administrator/adlogin.php
+++ administrator/adlogin.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/0594667512.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="CSS\register.css">
+    <link rel="stylesheet" href="..\CSS\register.css">
 
     <title>Login page</title>
   </head>
